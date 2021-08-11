addd1bc0443f857ab91830b6f3849dada135bcbd
Kol <hi@ttt.tl>
2021-08-11 06:48:10 +0000

    feat: add use parser util

---
+++ utils/use-parser.ts
@@ -0,0 +1,21 @@
+import md from 'marked'
+import fs from 'fs-extra'
+
+const useParser = async (path: string) => {
+  const parser = require('markdown-yaml-metadata-parser')
+  const raw = await fs.readFile(path, { encoding: 'utf-8' })
+  const output = await parser(raw)
+  const content = md(output.content)
+  const { title, date, tags } = output.metadata
+
+  return {
+    title,
+    date,
+    tags: tags || [],
+    content,
+    raw,
+    metadata: output.metadata
+  }
+}
+
+export default useParser
