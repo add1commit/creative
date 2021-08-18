edf44279ac5d7292dbaeb1ce1075173602579169
Kol <hi@ttt.tl>
2021-08-18 08:45:52 +0000

    fix: update utils

--- utils/index.ts
+++ utils/index.ts
@@ -1,13 +1,8 @@
 import path from 'path'
-import fs from 'fs-extra'
-import YAML from 'yaml'
 import type { Response } from 'express'
 import dayjs from 'dayjs'
 
 export const resolve = (arg: string) => path.join(__dirname, arg)
-
-const STATE_PATH = resolve('../creative.yaml')
-const output = fs.readFileSync(STATE_PATH, { encoding: 'utf-8' })
 
 export const composePromise = <T>(...fns: any) => {
   const init = fns.pop()
@@ -25,8 +20,6 @@ export const render = async (res: Response, page: string, options?: object | und
     res.render(`${page}`, options, (err: Error, html: string) => (err ? reject(err) : resolve(res.send(html))))
   })
 }
-
-export const state = YAML.parse(output)
 
 export const tools = {
   format: (date: string, pattern: string) => dayjs(date).format(pattern)
