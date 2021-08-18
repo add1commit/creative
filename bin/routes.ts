dce89e0e99f2fbf8ee29395c1c8528738a9797fd
Kol <hi@ttt.tl>
2021-08-18 05:30:42 +0000

    chore: change routes dir location

---
+++ bin/routes.ts
@@ -0,0 +1,46 @@
+import express, { Request, Response, Router } from 'express'
+import { Post, Posts, Page } from '../hooks'
+import { render } from '../utils'
+
+require('express-async-errors')
+
+class Routes {
+  public router: Router = express.Router()
+
+  constructor() {
+    this.router.get('/', this.renderMainPage)
+    this.router.get('/page/:num', this.renderMainPage)
+    this.router.get('/post/:category?/:name', this.renderPostPage)
+    this.router.get('/:custom', this.renderCustomPage)
+  }
+
+  private renderMainPage = async (req: Request, res: Response) => {
+    try {
+      const posts = new Posts(req.params.num)
+      await render(res, 'index', { posts })
+    } catch (error) {
+      res.render('error', { error })
+    }
+  }
+
+  private renderPostPage = async (req: Request, res: Response) => {
+    try {
+      const post = await Post(req.originalUrl)
+      await render(res, 'post', { post })
+    } catch (error) {
+      res.render('error', { error })
+    }
+  }
+
+  private renderCustomPage = async (req: Request, res: Response) => {
+    try {
+      const { params, originalUrl } = req
+      const page = await Page(originalUrl)
+      await render(res, `${params.custom}`, { page })
+    } catch (error) {
+      res.render('error', { error })
+    }
+  }
+}
+
+export default new Routes()
